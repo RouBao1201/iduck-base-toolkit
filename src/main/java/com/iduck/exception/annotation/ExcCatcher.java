c08a6ffe8b144e5401cfc19f9f843aae86319a84
“JieN” <624142800@qq.com>
2022-11-28 23:06:49 +0800

    自定义注解捕获异常02

--- src/main/java/com/iduck/exception/annotation/ExcCatcher.java
+++ src/main/java/com/iduck/exception/annotation/ExcCatcher.java
@@ -1,7 +1,11 @@
 package com.iduck.exception.annotation;
 
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 
 /**
